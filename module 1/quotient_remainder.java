a4d458497ad11f5b406bec12b4756a6344af6b7d
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-27 22:00:52 +0530

    quotient_remainder

---
+++ module 1/quotient_remainder.java
@@ -0,0 +1,9 @@
+class quotient_remainder{
+	public static void main(String args[])
+	{
+		int a = 5;
+		int b = 2;
+		System.out.println("quotient is"+ a/b);
+		System.out.println("remainder is"+ a%b);
+	}
+}
