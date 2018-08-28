37e1b31af5e5775d61a9cc273a8b912ddac709e2
KujithaMannepuri <kujitha120@msitprogram.net>
2018-08-28 19:05:26 +0530

    bigger_equal_smaller

---
+++ module 2/bigger_equal_smaller.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class bigger_equal_smaller{
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		int varA = scan.nextInt();
+		int varB = scan.nextInt();
+		if(varA > varB){
+			System.out.println("Bigger");
+		}
+		else if(varA < varB){
+			System.out.println("smaller");
+		}
+		else{
+			System.out.println("equal");
+		}
+	}
+}
